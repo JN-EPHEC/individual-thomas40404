04383224f5fd699cd1a397b1877036065084aa30
teamcharlier <200207490+thomas40404@users.noreply.github.com>
2026-03-17 15:00:05 +0100

    refactor: move TP05 to repo root

--- TP02/dev3-backend-tp2/dist/models/user.d.ts
+++
@@ -1,5 +0,0 @@
-import { Model } from "sequelize";
-declare class User extends Model {
-}
-export default User;
-//# sourceMappingURL=user.d.ts.map
--- TP03ccv/server/src/config/database.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite',
-  logging: console.log,
-});
-
-export default sequelize;
